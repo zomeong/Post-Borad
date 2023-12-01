033aff204c991e160e3bf29734a45ee0c06bf8c2
Kim Jihyun <legojhk@gmail.com>
2023-12-01 23:31:26 +0900

    test(userDetails) : userDetails Service 테스트

---
+++ src/test/java/com/sparta/post_board/service/UserDetailsServiceTest.java
@@ -0,0 +1,60 @@
+package com.sparta.post_board.service;
+
+import com.sparta.post_board.entity.User;
+import com.sparta.post_board.entity.UserRoleEnum;
+import com.sparta.post_board.repository.UserRepository;
+import com.sparta.post_board.security.UserDetailsServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+public class UserDetailsServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserDetailsServiceImpl userDetailsService;
+
+    @Test
+    void loadUserByUsernameTest1(){
+        // given
+        User user = new User("test user", "password", UserRoleEnum.USER);
+        when(userRepository.findByUsername("test user")).thenReturn(Optional.of(user));
+
+        // when
+        UserDetails userDetails = userDetailsService.loadUserByUsername("test user");
+
+        // then
+        assertEquals(user.getUsername(), userDetails.getUsername());
+        assertEquals(user.getPassword(), userDetails.getPassword());
+    }
+
+    @Test
+    void loadUserByUsernameTest2(){
+        // given
+        String username = "test user";
+        when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+        // when
+        Exception e = assertThrows(UsernameNotFoundException.class, () -> {
+            userDetailsService.loadUserByUsername(username);
+        });
+
+        // then
+        assertEquals("Not Found " + username, e.getMessage());
+    }
+}
