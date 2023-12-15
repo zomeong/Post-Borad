a021dd9dc680d0683f192ecfdf5589e9d354b013
Kim Jihyun <legojhk@gmail.com>
2023-12-15 23:48:51 +0900

    refactor : CustomException 정의

--- src/main/java/com/sparta/post_board/exception/DuplicateUserException.java
+++ src/main/java/com/sparta/post_board/exception/DuplicateUserException.java
@@ -1,7 +1,7 @@
 package com.sparta.post_board.exception;
 
 public class DuplicateUserException extends RuntimeException {
-    private static final String message = "중복된 유저입니다.";
+    private static final String message = "중복된 사용자가 존재합니다.";
 
     public DuplicateUserException(){
         super( message);
