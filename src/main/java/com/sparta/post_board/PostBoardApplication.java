bbd505db2f432f25f9c90aeaa3854f7912d6a280
Kim Jihyun <legojhk@gmail.com>
2023-12-18 18:59:48 +0900

    feat : parameter aop 적용

--- src/main/java/com/sparta/post_board/PostBoardApplication.java
+++ src/main/java/com/sparta/post_board/PostBoardApplication.java
@@ -2,9 +2,11 @@ package com.sparta.post_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class PostBoardApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/sparta/post_board/aop/ParameterAop.java
@@ -0,0 +1,46 @@
+package com.sparta.post_board.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+public class ParameterAop {
+
+    //적용 지점 설정
+    @Pointcut("execution(* com.sparta.post_board.contorller.*.*(..))")
+    private void cut() {}
+
+    //cut() 메서드가 실행 되는 지점 이전에 before() 메서드 실행
+    @Before("cut()")
+    public void before(JoinPoint joinPoint) {
+
+        //실행되는 함수 이름을 가져오고 출력
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        System.out.println(method.getName() + " 메서드 실행");
+
+        //메서드에 들어가는 매개변수 배열을 읽어옴
+        Object[] args = joinPoint.getArgs();
+
+        //매개변수 배열의 종류와 값을 출력
+        for(Object obj : args) {
+            System.out.println("type : "+ obj.getClass().getSimpleName());
+            System.out.println("value : "+ obj);
+        }
+    }
+
+    //cut() 메서드가 종료되는 시점에 afterReturn() 메서드 실행
+    //@AfterReturning 어노테이션의 returning 값과 afterReturn 매개변수 obj의 이름이 같아야 함
+    @AfterReturning(value = "cut()", returning = "obj")
+    public void afterReturn(JoinPoint joinPoint, Object obj) {
+        System.out.println("return obj : " + obj);
+    }
+}
